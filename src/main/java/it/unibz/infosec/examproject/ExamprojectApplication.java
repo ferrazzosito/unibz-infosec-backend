f6063a74721d7effc43c2c42148228b3ddf2d315
Andrea Esposito <aespositox@gmail.com>
2023-05-02 14:05:53 +0200

    Upload spring-booted project

---
+++ src/main/java/it/unibz/infosec/examproject/ExamprojectApplication.java
@@ -0,0 +1,13 @@
+package it.unibz.infosec.examproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unibz/infosec/examproject/ExamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unibz.infosec.examproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
