366aa9ab96c559a4fe60e944575429ca01bfe17f
Andrea Esposito <aespositox@gmail.com>
2023-05-18 17:21:44 +0200

    Implemented salt and password hashing in user management

--- src/main/java/it/unibz/infosec/examproject/ExamProjectApplication.java
+++ src/main/java/it/unibz/infosec/examproject/ExamProjectApplication.java
@@ -2,6 +2,7 @@ package it.unibz.infosec.examproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ExamProjectApplication {
