86a8f4c123f3489b17f3cabc2e542d936d70c87a
Andrea Esposito <aespositox@gmail.com>
2023-05-18 18:51:46 +0200

    Set spring datasource for postgres, adjusted model accordingly

--- src/main/java/it/unibz/infosec/examproject/ExamProjectApplication.java
+++ src/main/java/it/unibz/infosec/examproject/ExamProjectApplication.java
@@ -2,9 +2,11 @@ package it.unibz.infosec.examproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EntityScan("it.unibz.infosec.examproject")
 public class ExamProjectApplication {
 
 	public static void main(String[] args) {
