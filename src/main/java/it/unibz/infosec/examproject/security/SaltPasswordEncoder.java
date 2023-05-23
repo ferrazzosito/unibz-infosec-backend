8236cbc04503ec92ba6fdf4edbb511cc8d84b928
Andrea Esposito <aespositox@gmail.com>
2023-05-23 23:15:48 +0200

    Maybe solution to username in password encoder problem

--- src/main/java/it/unibz/infosec/examproject/security/SaltPasswordEncoder.java
+++ src/main/java/it/unibz/infosec/examproject/security/SaltPasswordEncoder.java
@@ -3,18 +3,22 @@ package it.unibz.infosec.examproject.security;
 import it.unibz.infosec.examproject.user.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class SaltPasswordEncoder implements PasswordEncoder {
 
+    @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    public SaltPasswordEncoder(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public SaltPasswordEncoder() {
     }
 
     @Override
     public String encode(CharSequence rawPassword) {
+        final String hopefullyUsername = ((ServletRequestAttributes) RequestContextHolder
+                .currentRequestAttributes())
+                .getRequest().getAttribute("username").toString();
         return null;
     }
 
