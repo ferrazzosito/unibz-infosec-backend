496ca704e3d40e76ea15e4f78c4abb8a50081a74
Francesco <francesco.mazzini432@gmail.com>
2023-05-24 14:10:00 +0200

    Login and signup are now working
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/main/java/it/unibz/infosec/examproject/security/SecurityConfig.java
+++ src/main/java/it/unibz/infosec/examproject/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package it.unibz.infosec.examproject.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +22,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private CustomAuthenticationProvider authProvider;
     private CustomUserDetailsService customUserDetailsService;
 
 //    private SaltPasswordEncoder passwordEncoder;
@@ -42,14 +46,22 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager
-            (AuthenticationConfiguration authenticationConfiguration ) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+//    @Bean
+//    public AuthenticationManager authenticationManager
+//            (AuthenticationConfiguration authenticationConfiguration ) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder () {
+//        return new SaltPasswordEncoder();
+//    }
 
     @Bean
-    public PasswordEncoder passwordEncoder () {
-        return new SaltPasswordEncoder();
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(authProvider);
+        return authenticationManagerBuilder.build();
     }
 }
